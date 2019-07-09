8cb5158fc8d863622d76f3c87bc9cf5a6fe57415
Frank Lünnemann <luennemann74@gmail.comgit config --global user.email luennemann74@gmail.com>
2019-07-09 12:20:18 +0200

    überarbeitet

--- main.js
+++ main.js
@@ -1,92 +1,79 @@
-﻿"use strict";
+"use strict";
 
-var utils = require(__dirname + '/lib/utils');
-var HID = require('usb-barcode-scanner');
+var utils = require('@iobroker/adapter-core');
+var adapterName = require(__dirname + '/package.json').name.split('.').pop();
+var adapterName = 'hid-barcode';
+var adapter = new utils.Adapter(adapterName);
 
-var hidDevice = null;
+var BS = require('usb-barcode-scanner').UsbScanner;
+var scanner = null;
 
-main()
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// States anlegen
+adapter.setObjectNotExists("RFID", {
+	type: "state",
+	common: {
+	name: "RFID",
+	type: "number",
+	role: "Zugang",
+	read: true,
+	write: true
+	},
+	native: {}
+	});
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var dev;
+adapter.on('ready', function(){
+main();
+});
 
-var sub = {
-    wo:     '',
-    double: '-double',
-    single: '-single-short',
-    long:   '-long',
-    action: '-action',
-    dsl:    '-dsl',
-    repcnt: '-repcnt',
-
-    raw: 'raw',
-    key: 'key'
-
-};
-
-var stateNames = {
-    Barcode:     { n: sub.raw,              val: '',     common: { write: false, name: 'Raw Key Code', desc: 'key-code, changes on first press'}}
-   
-};
-
-function createAll(callback) {
-
-    var hidDeviceName = '';
-    var d = HID.devices().find(function(d) {
-        return (d.vendorId == adapter.config.vendorID && d.productId == adapter.config.productID);
-    });
-    if (d) {
-        hidDeviceName = d.manufacturer + ' - ' + d.product;
-    }
-    dev = new devices.CDevice(adapter.config.vendorID + '-' + adapter.config.productID, hidDeviceName);
-
-    //for (var prefix in { raw: '', key: '' }) {
-        for (var i in stateNames) {
-            var st = Object.assign({}, stateNames[i]);
-            var n = st.n;
-            delete st.n;
-            dev.createNew(n, st);
-        }
-    //}
-    dev.update(callback);
-}
+adapter.on('unload', function(){
+scanner.stopScanning();
+});
 
 
 
+//###########################################   Main Funktion   ############################################################################
 function main() {
-/*
-    normalizeConfig(adapter.config);
-    mappings = adapter.ioPack.mappings;
-    if (!adapter.config.vendorID || !adapter.config.productID) {
+
+  if (!adapter.config.vendorID || !adapter.config.productID) {
         adapter.log.error("VendorID and ProductID has to be configured");
         return;
     }
 
     try {
-        hidDevice = new HID.HID(adapter.config.vendorID, adapter.config.productID);
+	adapter.log.info (adapter.config.vendorID + "    " +  adapter.config.productID)
+
+      scanner = new BS({vendorId: Number(adapter.config.vendorID), productId: Number(adapter.config.productID) });
+
     } catch(e) {
         adapter.log.error(e.message);
-        adapter.log.error('If running on Windows, see requirements in readme.md. https://github.com/soef/iobroker.hid/blob/master/README.md');
+        return;
+
+    }
+    if (!scanner) {
+        adapter.log.error("kann Gerät nicht öffnen VendorID " + adapter.config.vendorID + " und Produkt ID " + adapter.config.productID);
         return;
     }
-    if (!hidDevice) {
-        adapter.log.error("can not open device with VendorID " + adapter.config.vendorID + " and Product ID " + adapter.config.productID);
-        adapter.log.error('If running on Windows, see requirements in readme.md. https://github.com/soef/iobroker.hid/blob/master/README.md');
-        return;
-    }
-    createAll();
 
-    hidDevice.on("data", function (data) {
-        var sData = data.toString('hex').toUpperCase();
-        onData(sData);
-    });
-    hidDevice.on("error", function (err) {
-        console.log("err: " + err);
-    });
-*/  
-  //adapter.subscribeStates('*');
-}
 
-//--msvs_version=2015
+scanner.on("data", function(data) {
+	adapter.setState("RFID", data, true); 
+	adapter.log.info("testdata " + data);
+});
+
+
+scanner.on('error', function(err){
+adapter.log.error(err)
+});
+
+ 
+scanner.startScanning();
+
+
+//adapter.subscribeStates('*');
+
+
+};
+
